8f3029998a2edd082806fbf99122b96711c7afab
Murphy <matt.griffiths@gmail.com>
2019-09-15 22:34:31 -0700

    Update gobuster.fish

--- fish_functions/gobuster.fish
+++ fish_functions/gobuster.fish
@@ -1,3 +1,4 @@
 function gobuster $argv
-  dir -w /opt/Wordlists/danielmiessler/Discovery/Web-Content/raft-large-words.txt -s 200,300,307 -t 100 -b 2019 --wildcard -u $argv
+  echo "dir -w /opt/Wordlists/danielmiessler/Discovery/Web-Content/raft-large-words.txt -s 200,300,307 -t 100 -b 2019 --wildcard -u $argv[1]"
+  dir -w /opt/Wordlists/danielmiessler/Discovery/Web-Content/raft-large-words.txt -s 200,300,307 -t 100 -b 2019 --wildcard -u $argv[1]
 end
