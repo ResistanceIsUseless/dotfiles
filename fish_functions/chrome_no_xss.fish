29395cf2d377b590ca06c59fbebccf7e4ac8bc0d
Murphy <matt.griffiths@gmail.com>
2019-09-15 18:16:48 -0700

    Create chrome_no_xss.fish

---
+++ fish_functions/chrome_no_xss.fish
@@ -0,0 +1,3 @@
+function chrome
+ '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' --disable-xss-auditor --enable-devtools-experiments --disable-features=enable-automatic-password-saving 2> /dev/null &
+end
