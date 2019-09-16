ab6d1b340edb7e9513a547f3724c3204b5e5494c
Murphy <matt.griffiths@gmail.com>
2019-09-15 18:57:49 -0700

    Create dirsearch.fish

---
+++ fish_functions/dirsearch.fish
@@ -0,0 +1,4 @@
+function dirsearch $argv
+  echo "python3 /opt/dirsearch/dirsearch.py -u $argv -e asp,aspx,htm,html,jsp,php,pl,cgi --random-agents -x 500,501,503,404"
+  python3 /opt/dirsearch/dirsearch.py -u $argv -e asp,aspx,htm,html,jsp,php,pl,cgi --random-agents -x 500,501,503,404
+end
