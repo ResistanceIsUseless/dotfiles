df0a14b79b4ddadfae51414769cd918541d4f365
Murphy <matt.griffiths@gmail.com>
2019-09-15 22:38:51 -0700

    Update http-servchk.fish

--- fish_functions/http-servchk.fish
+++ fish_functions/http-servchk.fish
@@ -1,3 +1,3 @@
-function findsubs --description 'http-servck path-to/input.txt path-to/output.txt' --argument-names input output 
-  sudo nmap -sSV -Pn -T5 -p 80,443,808,1080,3128,4180,4443,4480,5490,7443,8000-8088,8443 -iL testbs.txt $input -oG $output
+function http-servchk --description 'http-servck path-to/input.txt path-to/output.txt' --argument-names input output 
+  sudo nmap -sSV -Pn -T5 -p 80,443,808,1080,3128,4180,4443,4480,5490,7443,8000-8088,8443,8888 -iL testbs.txt $input -oN $output
 end
