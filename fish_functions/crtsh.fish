cf57cc6b2acae2c9d6db3775c52aa3ae029ca071
Murphy <matt.griffiths@gmail.com>
2019-09-23 15:54:48 -0700

    Update crtsh.fish

--- fish_functions/crtsh.fish
+++ fish_functions/crtsh.fish
@@ -1,4 +1,4 @@
 function crtsh --description 'search cert.sh for subdomains' --argument-names host1 
-    command curl -s "https://crt.sh/?q=%.$host1"  | sed 's/<\/\?[^>]\+>//g' | grep $host1
+    command curl -s "https://crt.sh/?q=%.$host1"  | awk '{gsub("<[^>]*>", "")}1' | grep $host1
 end
-#needs work to remove html
+
