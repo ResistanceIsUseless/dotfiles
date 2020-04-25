79226d1bb6217a37e8c9acbdba194c60b855239d
Murphy <matt.griffiths@gmail.com>
2020-04-25 11:56:33 -0700

    Update whatweb.fish

--- fish_functions/whatweb.fish
+++ fish_functions/whatweb.fish
@@ -1,4 +1,4 @@
-function whatweb --argument-names input output
-     echo "/opt/Whatweb/whatweb -a 3 -i $input --log-brief=$output"
-     /opt/Whatweb/whatweb -a 3 -i $input --log-brief=$output
+function f_whatweb --argument-names input output
+     echo "whatweb -a 3 -i $input --log-brief=$output"
+     f_whatweb -a 3 -i $input --log-brief=$output
 end
