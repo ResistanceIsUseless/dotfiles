fb6f6735084e8221738d425b1f7ca4086c14c943
Murphy <matt.griffiths@gmail.com>
2020-04-25 12:50:16 -0700

    Update waybackrobots.fish

--- fish_functions/waybackrobots.fish
+++ fish_functions/waybackrobots.fish
@@ -1,4 +1,4 @@
-function waybackrobots --argument-names input 
-    echo "python3 /opt/waybackrobots/waybackrobots.py $input"
-    python3 /opt/waybackrobots/waybackrobots.py $input
+function ff.waybackrobots --argument-names input 
+    echo "python3 /home/static/Tools/waybackrobots/waybackrobots.py $input"
+    python3 /home/static/Tools/waybackrobots/waybackrobots.py $input
 end
