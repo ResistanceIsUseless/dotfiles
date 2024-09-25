fd88807b3fbb7842b19b6156fcdef20b2dc645ca
StaticBunny <matt.griffiths@gmail.com>
2024-09-24 19:03:13 -0700

    nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,58 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
+-- Set cursor shape
+vim.opt.guicursor = { "n-v-c-i:block" }
 
+-- Line wrapping
+vim.opt.wrap = false
+vim.opt.linebreak = true
+vim.opt.showbreak = '▹'
+
+-- Auto indent
+vim.opt.autoindent = true
+
+-- Searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.gdefault = true
+vim.opt.hlsearch = true
+vim.opt.showmatch = true
+
+-- Enable jumping into files in a search buffer
+vim.opt.hidden = true
+
+-- Backspace settings
+vim.opt.backspace = { "eol", "start", "indent" }
+
+-- Indentation settings
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+-- Direction keys for wrapped lines
+vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Up>', 'gk', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Down>', 'gj', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Up>', '<Esc>gka', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Down>', '<Esc>gja', { noremap = true, silent = true })
+
+-- Bash / Emacs keys for command line
+vim.api.nvim_set_keymap('c', '<C-a>', '<Home>', { noremap = true })
+vim.api.nvim_set_keymap('c', '<C-e>', '<End>', { noremap = true })
+
+-- Base64 decode word under cursor
+vim.api.nvim_set_keymap('n', '<Leader>b', ":!echo <C-R><C-W> | base64 -d<CR>", { noremap = true })
+
+-- Grep recursively for word under cursor
+vim.api.nvim_set_keymap('n', '<Leader>g', ":tabnew|read !grep -Hnr '<C-R><C-W>'<CR>", { noremap = true })
+
+-- Enable wildmenu for command completion
+vim.opt.wildmenu = true
+
+-- Write current file with sudo permissions
+vim.api.nvim_create_user_command('W', 'w !sudo tee % > /dev/null', {})
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
